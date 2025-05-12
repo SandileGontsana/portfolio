2caa7ae72596efd49452b7e515c92b602c9d705d
SandileGontsana <sandilegontsana2@gmail.com>
2025-05-12 13:06:15 +0200

    Linkedin link fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -216,7 +216,7 @@ let footer = $(`
     </svg>
     </a>
 
-    <a class="social-button linkedin" href="" target="_blank" >
+     <a class="socialicon linkedin"  href="https:www.linkedin.com/in/sandile-gontsana-165147235" target="_blank" rel="author">
     <!-- svg code for linkedin icon -->
       <svg class="linkedin-icon-footer" xmlns="http://www.w3.org/2000/svg" width="35" viewBox="0 0 24 24" fill="#0e76a8"
         stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin">
